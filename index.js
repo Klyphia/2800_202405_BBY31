7269496cec7af17791845bd7cf68020b8bd9b3cc
skcalvin <calvinhuango3o@gmail.com>
2024-05-14 11:57:21 -0700

    CREATED .env file with mongo database secrets and ADDED mongodb connection code in index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const MongoStore = require('connect-mongo');
+
+/* secret stuff */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* secret stuff */
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
