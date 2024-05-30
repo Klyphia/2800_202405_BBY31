93ff1ac238b91d626ef4783a7fbc1072d5706a7c
skcalvin <calvinhuango3o@gmail.com>
2024-05-29 19:24:05 -0700

    Added a 404 page

--- index.js
+++ index.js
@@ -901,10 +901,13 @@ app.post("/saveMood", sessionValidation, async (req, res) =>{
   } catch (error) {
     console.error("Error saving mood:", error);
     res.status(500).json({ message: "Internal server error" });
-  }
-
-  
+  }    
 });
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
